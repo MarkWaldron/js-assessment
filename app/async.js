0e46e6d6e454e935bb6d7ce5fa8c11780df26651
MarkWaldron <markewaldron@gmail.com>
2016-04-07 23:34:04 -0400

    Added solutions for async tests

--- app/async.js
+++ app/async.js
@@ -2,10 +2,34 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    var result = new Promise(function(resolve, reject){
+      if(value){
+        return resolve(value)
+      } else {
+        return reject(Error("False"))
+      }
+    })
+    return result;
   },
 
   manipulateRemoteData : function(url) {
+    var parse = function(response){
+      return response.json();
+    };
 
+    var sortJSON = function(data){
+      var arr = [];
+      for(var key in data){
+        if(typeof data[key] == 'object') {
+          arr = arr.concat(sortJSON(data[key]));
+        } else {
+          arr.push(data[key])
+        }
+      }
+      return arr.sort();
+    }
+
+    var result = fetch(url).then(parse);
+    return result.then(sortJSON);
   }
 };
