4624880edc8d254cc3c8035e00206e7bc8f6b95c
MarkWaldron <markewaldron@gmail.com>
2016-04-11 21:28:56 -0400

    Added solutions for count and flowControl

--- app/count.js
+++ app/count.js
@@ -2,6 +2,20 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var to;
+    function startCount() {
+      console.log(start++);
 
+      if (start <= end) {
+        to = setTimeout(startCount, 100);
+      }
+    }
+    startCount();
+
+    return {
+      cancel: function(){
+        to && clearTimeout(to)
+      }
+    };
   }
 };
